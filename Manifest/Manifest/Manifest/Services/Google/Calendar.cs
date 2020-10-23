fdf4dea72c2fb50cd706d75ff7bbd9f566cb54c3
ashutosh <ashutoshmahala@live.com>
2020-10-23 14:14:22 -0700

    CleanUp(Google Calendar) : Removed dead code and moved url to constants file

--- Manifest/Manifest/Manifest/Services/Google/Calendar.cs
+++ Manifest/Manifest/Manifest/Services/Google/Calendar.cs
@@ -15,35 +15,6 @@ namespace Manifest.Services.Google
     public class Calendar : ICalendarClient
     {
         HttpClient client = new HttpClient();
-        public async Task<bool> UseAccessToken(string accessToken)
-        {
-
-            //Make HTTP Request
-            var request = new HttpRequestMessage();
-            request.RequestUri = new Uri(Constants.GoogleUserInfoUrl);
-            request.Method = HttpMethod.Get;
-
-            //Format Headers of Request with included Token
-            //string bearerString = string.Format("Bearer {0}", Application.Current.Properties["access_token"].ToString());
-            string bearerString = string.Format("Bearer {0}", accessToken);
-            request.Headers.Add("Authorization", bearerString);
-            request.Headers.Add("Accept", "application/json");
-            var client = new HttpClient();
-            HttpResponseMessage response = await client.SendAsync(request);
-            HttpContent content = response.Content;
-            var json = await content.ReadAsStringAsync();
-            JObject jsonParsed = JObject.Parse(json);
-
-            if (jsonParsed["error"] != null)
-                return false;
-
-            return true;
-        }
-
-        //public async Task<bool> GetEventsForDay(string accessToken, DateTime dateTime)
-        //{
-
-        //}
 
         public async Task<List<Event>> GetEventsList(string accessToken, DateTimeOffset dateTimeOffset)
         {
@@ -56,12 +27,9 @@ namespace Manifest.Services.Google
             string[] timeZoneOffsetParsed = timeZoneOffset.Split('-');
             int timeZoneNum = Int32.Parse(timeZoneOffsetParsed[1].Substring(0, 2));
 
-            DateTime currentTimeinUTC = DateTime.Now.ToUniversalTime();
-            int uTCHour = (currentTimeinUTC.Hour - timeZoneNum);
-            int currentLocalUTCMinute = currentTimeinUTC.Minute;
 
             //Make HTTP Request
-            string baseUri = "https://www.googleapis.com/calendar/v3/calendars/primary/events?orderBy=startTime&singleEvents=true&";
+            string baseUri = Constants.GoogleCalendarUrl + "?orderBy=startTime&singleEvents=true&";
 
             string monthString;
             string dayString;
@@ -110,41 +78,6 @@ namespace Manifest.Services.Google
             return events;
         }
 
-        //public async Task<bool> LoadTodaysEvents(string accessToken, DateTime dateTime, DateTimeOffset dateTimeOffset)
-        //{
-        //    var jsonResult = await GetAllTodaysEventsList(accessToken, dateTime, dateTimeOffset);
-
-        //    //Return error if result is empty
-        //    if (jsonResult == null)
-        //    {
-        //        return false;
-        //    }
-
-        //    //Parse the json using EventsList Method
-
-        //    try
-        //    {
-
-        //        var parsedResult = JsonConvert.DeserializeObject<EventDto>(jsonResult);
-
-        //        //Separate out just the EventName
-        //        foreach (EventDto events in parsedResult.Items)
-        //        {
-        //            App.User.CalendarEvents.Add(events);
-        //            Console.WriteLine(events.EventName.ToString());
-        //        }
-        //    }
-        //    catch (NullReferenceException e)
-        //    {
-        //        //LoginPage.access_token = LoginPage.refreshToken;
-        //        //await Navigation.PopAsync();
-        //        //Console.WriteLine(LoginPage.access_token);
-        //        return false;
-        //    }
-
-        //    return true;
-        //}
-
 
         private async Task<string> MakeEventsRequest(string uri, string accessToken)
         {
@@ -157,12 +90,14 @@ namespace Manifest.Services.Google
             request.Headers.Add("Authorization", bearerString);
             request.Headers.Add("Accept", "application/json");
 
+            Debug.WriteLine("Manifest.Services.Google.Calendar: Making request to "+ uri);
+
             Task<HttpResponseMessage> responseTask = client.SendAsync(request);
             responseTask.Wait();
             HttpResponseMessage response = responseTask.Result;
             HttpContent content = response.Content;
             var json = await content.ReadAsStringAsync();
-            Debug.WriteLine("Google.Calendar.Response:\n" + json);
+            Debug.WriteLine("Manifest.Services.Google.Calendar.Response:\n" + json);
             return json;
         }
     }
