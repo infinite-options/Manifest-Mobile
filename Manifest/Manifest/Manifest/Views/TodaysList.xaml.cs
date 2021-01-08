a251106972ee4b0583a5cb73cebe597701a397fd
AdarshSekar04 <adarsh.sekar04@gmail.com>
2021-01-07 17:12:05 -0800

    Publishing to MyLife. Remembeer to change setting back to MySpace

--- Manifest/Manifest/Manifest/Views/TodaysList.xaml.cs
+++ Manifest/Manifest/Manifest/Views/TodaysList.xaml.cs
@@ -227,8 +227,8 @@ namespace Manifest.Views
             {
                 Text = DateTime.Now.DayOfWeek.ToString(),
                 FontSize = 40,
-                Padding = new Thickness(0, 40, 0, 0),
-                Margin = new Thickness(0, 40, 0, 0)
+                Padding = new Thickness(0, 80, 0, 0),
+                //Margin = new Thickness(0, 80, 0, 0)
             };
             LoadGroups(new ObservableCollection<TodaysListTileGroup>());
         }
