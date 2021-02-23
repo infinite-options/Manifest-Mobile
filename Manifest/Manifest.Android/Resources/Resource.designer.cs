e1de7e3ed40fe952d89a1115478eccc1fe3471d4
AdarshSekar04 <adarsh.sekar04@gmail.com>
2021-02-23 14:10:00 -0800

    Added phone button on events page

--- Manifest/Manifest.Android/Resources/Resource.designer.cs
+++ Manifest/Manifest.Android/Resources/Resource.designer.cs
@@ -19461,34 +19461,37 @@ namespace Manifest.Droid
 			public const int personIcon = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int progBar = 2131165365;
+			public const int phone = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int rightArrow = 2131165366;
+			public const int progBar = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int settingsIcon = 2131165367;
+			public const int rightArrow = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int sublist = 2131165368;
+			public const int settingsIcon = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int sunriselow = 2131165369;
+			public const int sublist = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int sunrisemid = 2131165370;
+			public const int sunriselow = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int test_custom_background = 2131165371;
+			public const int sunrisemid = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int tooltip_frame_dark = 2131165372;
+			public const int test_custom_background = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int tooltip_frame_light = 2131165373;
+			public const int tooltip_frame_dark = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int yellowclock = 2131165374;
+			public const int tooltip_frame_light = 2131165374;
+			
+			// aapt resource value: 0x7F0700BF
+			public const int yellowclock = 2131165375;
 			
 			static Drawable()
 			{
