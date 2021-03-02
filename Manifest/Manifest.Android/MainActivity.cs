4057387da44928025c7082bec9371131d960986f
Carlos <omarfacio2010@gmail.com>
2021-03-02 03:48:48 -0800

    Manifest MySpace latest app

--- Manifest/Manifest.Android/MainActivity.cs
+++ Manifest/Manifest.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Android.Util;
 
 namespace Manifest.Droid
 {
-    [Activity(Label = "Manifest", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Manifest MySpace", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
