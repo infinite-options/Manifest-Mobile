d4c4b5dee10b911a06c3a034b6258a2fc8048aaf
Carlos <omarfacio2010@gmail.com>
2021-03-02 03:05:34 -0800

    Progress page is done

--- Manifest/Manifest/Views/ProgressPage.xaml.cs
+++ Manifest/Manifest/Views/ProgressPage.xaml.cs
@@ -26,13 +26,20 @@ namespace Manifest.Views
     public class ProgressRow
     {
         public string name { get; set; }
-        public string statusA { get; set; }
-        public string statusB { get; set; }
-        public string statusC { get; set; }
-        public string statusD { get; set; }
-        public string statusE { get; set; }
-        public string statusF { get; set; }
-        public string statusG { get; set; }
+        public string strokeColorA {get;set;}
+        public bool fillA { get; set; }
+        public string strokeColorB { get; set; }
+        public bool fillB { get; set; }
+        public string strokeColorC { get; set; }
+        public bool fillC { get; set; }
+        public string strokeColorD { get; set; }
+        public bool fillD { get; set; }
+        public string strokeColorE { get; set; }
+        public bool fillE { get; set; }
+        public string strokeColorF { get; set; }
+        public bool fillF { get; set; }
+        public string strokeColorG { get; set; }
+        public bool fillG { get; set; }
     }
 
     public partial class ProgressPage : ContentPage
@@ -67,81 +74,171 @@ namespace Manifest.Views
 
         async void GetUserProgress()
         {
-            var client = new HttpClient();
-            var startDate = DateTime.Now;
-            var endDate = DateTime.Now;
-            var userId = (string)Application.Current.Properties["userId"];
-            for(int i = 7; i > 0; i--)
+            try
             {
-                startDate = startDate.AddDays(-1);
-            }
-            Debug.WriteLine("START: " + startDate);
-            Debug.WriteLine("END: " + endDate);
-
-            dates.Add(new HeaderInfo() { name = "Goals", date = "" });
-            for(int i = 0; i < 7; i++)
-            {
-                var temp = startDate.AddDays(i);
-
-                dates.Add(new HeaderInfo() { name = temp.ToString("ddd"), date = temp.ToString("MM/dd") });
-            }
-
-            DatesList.ItemsSource = dates;
-
-            client.DefaultRequestHeaders.Add("start-date", startDate.ToString("yyyy-MM-dd"));
-            client.DefaultRequestHeaders.Add("end-date", endDate.ToString("yyyy-MM-dd"));
-
-            var response = await client.GetAsync("https://3s3sftsr90.execute-api.us-west-1.amazonaws.com/dev/api/v2/goalRoutineHistory/" + userId);
-
-            if (response.IsSuccessStatusCode)
-            {
-                
-                var data = await response.Content.ReadAsStringAsync();
-                var user = JsonConvert.DeserializeObject<History>(data);
-                Debug.WriteLine("HISTORY DATA: " + user.result);
-                var dates = JsonConvert.DeserializeObject<IDictionary<string, object>>(user.result.ToString());
-
-
-
-                var dateItems = new List<string>();
-                var goalsItems = new List<string>();
-
-                
-                foreach (string key in dates.Keys)
+                var client = new HttpClient();
+                var startDate = DateTime.Now;
+                var endDate = DateTime.Now;
+                var userId = (string)Application.Current.Properties["userId"];
+                for (int i = 7; i > 0; i--)
                 {
-                    dateItems.Add(key);
+                    startDate = startDate.AddDays(-1);
+                }
+                Debug.WriteLine("START: " + startDate);
+                Debug.WriteLine("END: " + endDate);
+
+                dates.Add(new HeaderInfo() { name = "Goals", date = "" });
+                for (int i = 0; i < 7; i++)
+                {
+                    var temp = startDate.AddDays(i);
+
+                    dates.Add(new HeaderInfo() { name = temp.ToString("ddd"), date = temp.ToString("MM/dd") });
                 }
 
-                if(dates.Keys.Count != 0)
+                DatesList.ItemsSource = dates;
+
+                client.DefaultRequestHeaders.Add("start-date", startDate.ToString("yyyy-MM-dd"));
+                client.DefaultRequestHeaders.Add("end-date", endDate.ToString("yyyy-MM-dd"));
+
+                var response = await client.GetAsync("https://3s3sftsr90.execute-api.us-west-1.amazonaws.com/dev/api/v2/goalRoutineHistory/" + userId);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    var subSet = JsonConvert.DeserializeObject<IDictionary<string, object>>(dates[dateItems[0]].ToString());
-                    foreach (string key in subSet.Keys)
+
+                    var data = await response.Content.ReadAsStringAsync();
+                    var user = JsonConvert.DeserializeObject<History>(data);
+                    Debug.WriteLine("HISTORY DATA: " + user.result);
+                    var dates = JsonConvert.DeserializeObject<IDictionary<string, object>>(user.result.ToString());
+
+
+
+                    var dateItems = new List<string>();
+                    var goalsItems = new List<string>();
+
+
+                    foreach (string key in dates.Keys)
                     {
-                        goalsItems.Add(key);
+                        dateItems.Add(key);
                     }
-                }
+                    var factor = 0;
+                    foreach (string key in dates.Keys)
+                    {
+                        var subSet = JsonConvert.DeserializeObject<IDictionary<string, object>>(dates[key].ToString());
+                        if (factor <= subSet.Count)
+                        {
+                            goalsItems.Clear();
+                            foreach (string subKey in subSet.Keys)
+                            {
+                                goalsItems.Add(subKey);
+                            }
+                        }
+                    }
 
-                var grid = new int[dateItems.Count,goalsItems.Count];
-                foreach(string key in dates.Keys)
+                    var grid = new List<List<int>>();
+                    foreach (string key in dates.Keys)
+                    {
+                        var item = JsonConvert.DeserializeObject<IDictionary<string, object>>(dates[key].ToString());
+                        var row = new List<int>();
+                        foreach (string subKey in goalsItems)
+                        {
+                            var value = 0;
+                            if (item.ContainsKey(subKey))
+                            {
+                                if (item[subKey].ToString() == "not started")
+                                {
+                                    value = -1;
+                                }
+                                else if (item[subKey].ToString() == "in_progress")
+                                {
+                                    value = 0;
+                                }
+                                else if (item[subKey].ToString() == "completed")
+                                {
+                                    value = 1;
+                                }
+                            }
+                            else
+                            {
+                                value = -1;
+                            }
+                            row.Add(value);
+                        }
+                        grid.Add(row);
+                    }
+
+                    Debug.WriteLine("ELEMENTS IN GRID: " + grid.Count);
+                    Debug.WriteLine("ELEMENTS IN GRID: " + grid[0].Count);
+
+                    int j = 0;
+                    rows.Clear();
+                    foreach (string key in goalsItems)
+                    {
+                        rows.Add(new ProgressRow()
+                        {
+                            name = key,
+                            strokeColorA = StrokeFill(grid[0][j]),
+                            fillA = Fill(grid[0][j]),
+                            strokeColorB = StrokeFill(grid[1][j]),
+                            fillB = Fill(grid[1][j]),
+                            strokeColorC = StrokeFill(grid[2][j]),
+                            fillC = Fill(grid[2][j]),
+                            strokeColorD = StrokeFill(grid[3][j]),
+                            fillD = Fill(grid[3][j]),
+                            strokeColorE = StrokeFill(grid[4][j]),
+                            fillE = Fill(grid[4][j]),
+                            strokeColorF = StrokeFill(grid[5][j]),
+                            fillF = Fill(grid[5][j]),
+                            strokeColorG = StrokeFill(grid[6][j]),
+                            fillG = Fill(grid[6][j]),
+
+                        }); ;
+                        j++;
+                    }
+
+                    GoalsStatusList.ItemsSource = rows;
+                }
+                else
                 {
-
+                    await DisplayAlert("Oops", "We weren't able to fulfill your request. Please try again later", "OK");
                 }
-
-                foreach(string key in goalsItems)
-                {
-
-                    rows.Add(new ProgressRow() { name = key });
-                }
-
-
-                GoalsStatusList.ItemsSource = rows;
-
-            }
-            else
+            }catch(Exception progress)
             {
-                await DisplayAlert("Oops","We weren't able to fulfill your request. Please try again later","OK");
+                await DisplayAlert("Oops", progress.Message, "OK");
             }
+        }
 
+        bool Fill(int val)
+        {
+            bool color = false;
+            if(val == -1)
+            {
+                color = false;
+            }else if (val == 0)
+            {
+                color = false;
+            }else if (val == 1)
+            {
+                color = true;
+            }
+            return color;
+        }
+
+        string StrokeFill(int val)
+        {
+            string color = "#00000059";
+            if (val == -1)
+            {
+                color = (string)Application.Current.Properties["goal"];
+            }
+            else if (val == 0)
+            {
+                color = (string)Application.Current.Properties["routine"];
+            }
+            else if (val == 1)
+            {
+                color = (string)Application.Current.Properties["event"];
+            }
+            return color;
         }
 
         void Button_Clicked(System.Object sender, System.EventArgs e)
